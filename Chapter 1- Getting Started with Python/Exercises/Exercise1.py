483bb43e02ead2a63d9b98724e9cc04df471e93c
Shuaaa-j <JoshuaGofulco246@gmail.com>
2023-09-26 23:19:21 -0700

    Update Exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise1.py
@@ -3,4 +3,4 @@ print ("""Twinkle, twinkle, little star,
               Up above the world so high,
                 Like a diamond in the sky.
              Twinkle, twinkle, little star,
-          How I wonder what you are!""")
+           How I wonder what you are!""")    
