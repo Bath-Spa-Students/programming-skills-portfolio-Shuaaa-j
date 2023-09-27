dbf87538e57c177225d5690e81ae3c63ddf47866
Shuaaa-j <JoshuaGofulco246@gmail.com>
2023-09-27 13:44:32 +0400

    Create Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,5 @@
+first_name = "Joshua"
+last_name = "Gofulco"
+favorite_language = "Python"
+
+print ("Hi my name is" + " " + first_name + " " + last_name + " " + "and my favorite language is"  + " " + favorite_language + "!")
